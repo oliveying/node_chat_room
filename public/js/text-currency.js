f559524e88b8910191946bb3311f6de24bf7d031
songxiaoying <songxiaoying>
2019-10-27 10:38:03 +0800

    complate chat

---
+++ public/js/text-currency.js
@@ -0,0 +1,6 @@
+var currency = require('./currency');
+
+console.log('50 canadian dollars equals this amount of us dollars;');
+console.log(currency.canadianToUs(50));
+
+console.log('30 us dollars equals this amount of canadian dollars')
